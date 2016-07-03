7e64a6c4338292dd0c6b767a938436d77a7ebab1
Scott Stamp <scott@hypermine.com>
2016-07-03 03:30:54 -0230

    Added SHIFT_BSPC, program.bat

--- keyboards/kc60/keymaps/scott/keymap.c
+++ keyboards/kc60/keymaps/scott/keymap.c
@@ -8,15 +8,15 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = KEYMAP( /* Basic QWERTY */
-      F(0),     KC_1,     KC_2,     KC_3,    KC_4,     KC_5,     KC_6,     KC_7,    KC_8,  KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  \
+      F(0),     KC_1,     KC_2,     KC_3,    KC_4,     KC_5,     KC_6,     KC_7,    KC_8,  KC_9,     KC_0,     KC_MINS,  KC_EQL,   F(3),     \
       KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,     KC_Y,     KC_U,    KC_I,  KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  \
       MO(1),    KC_A,     KC_S,     KC_D,    KC_F,     KC_G,     KC_H,     KC_J,    KC_K,  KC_L,     KC_SCLN,  KC_QUOT,  KC_NO,    KC_ENT,   \
       KC_LSFT,  KC_NO,    KC_Z,     KC_X,    KC_C,     KC_V,     KC_B,     KC_N,    KC_M,  KC_COMM,  KC_DOT,   KC_SLSH,  KC_NO,    KC_RSFT,  \
-      KC_LCTL,  KC_LGUI,  KC_LALT,                     KC_SPC,                                       KC_RALT,  KC_RGUI,  MO(1),    KC_RCTL \
+      KC_LCTL,  KC_LGUI,  KC_LALT,                     KC_SPC,                                       KC_RALT,  MO(1),    KC_RGUI,  KC_RCTL \
       ),
   [1] = KEYMAP(
       F(0),     KC_F1,    KC_F2,    KC_F3,   KC_F4,    KC_F5,    KC_F6,    KC_F7,   KC_F8,  KC_F9,   KC_F10,   KC_F11,   KC_F12,   KC_DEL,   \
-      KC_TRNS,  F(1),     KC_UP,    F(2),    KC_PGUP,  KC_TRNS,  KC_PGUP,  KC_HOME, KC_UP,  KC_END,  KC_PSCR,  KC_TRNS,  KC_TRNS,  KC_TRNS,  \
+      KC_TRNS,  F(1),     KC_UP,    F(2),    KC_PGUP,  KC_TRNS,  KC_PGUP,  F(1),    KC_UP,  F(2),    KC_PSCR,  KC_TRNS,  KC_TRNS,  KC_TRNS,  \
       KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RGHT, KC_PGDN,  KC_TRNS,  KC_PGDN,  KC_LEFT, KC_DOWN,KC_RGHT, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  \
       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,KC_VOLD, KC_VOLU,  KC_MUTE,  KC_TRNS,  KC_TRNS,  \
       KC_TRNS,  KC_TRNS,  KC_TRNS,                     KC_TRNS,                                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS \
@@ -27,12 +27,14 @@ enum function_id {
 	SHIFT_ESC,
 	SHIFT_HOME,
 	SHIFT_END,
+    SHIFT_BSPC,
 };
 
 const uint16_t PROGMEM fn_actions[] = {
 	[0] = ACTION_FUNCTION(SHIFT_ESC),
 	[1] = ACTION_FUNCTION(SHIFT_HOME),
 	[2] = ACTION_FUNCTION(SHIFT_END),
+    [3] = ACTION_FUNCTION(SHIFT_BSPC),
 };
 
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
@@ -98,5 +100,25 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
 				}
 			}
 			break;
+        case SHIFT_BSPC:
+            shift_mask = get_mods()&MODS_CTRL_MASK;
+            if (record->event.pressed) {
+                if (shift_mask) {
+                    add_key(KC_DEL);
+                    send_keyboard_report();
+                } else {
+                    add_key(KC_BSPC);
+                    send_keyboard_report();
+                }
+            } else {
+                if (shift_mask) {
+                    del_key(KC_DEL);
+                    send_keyboard_report();
+                } else {
+                    del_key(KC_BSPC);
+                    send_keyboard_report();
+                }
+            }
+            break;
 	}
 }
