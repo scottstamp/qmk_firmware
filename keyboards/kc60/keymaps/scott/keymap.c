ea3af823231947ef2aac3b5e73a3415e87520660
Scott Stamp <scott@hypermine.com>
2016-07-03 02:41:53 -0230

    Fixed SHIFT_HOME, SHIFT_END

--- keyboards/kc60/keymaps/scott/keymap.c
+++ keyboards/kc60/keymaps/scott/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       ),
   [1] = KEYMAP(
       F(0),     KC_F1,    KC_F2,    KC_F3,   KC_F4,    KC_F5,    KC_F6,    KC_F7,   KC_F8,  KC_F9,   KC_F10,   KC_F11,   KC_F12,   KC_DEL,   \
-      KC_TRNS,  KC_HOME,  KC_UP,    KC_END,  KC_PGUP,  KC_TRNS,  KC_PGUP,  KC_HOME, KC_UP,  KC_END,  KC_PSCR,  KC_TRNS,  KC_TRNS,  KC_TRNS,  \
+      KC_TRNS,  F(1),     KC_UP,    F(2),    KC_PGUP,  KC_TRNS,  KC_PGUP,  KC_HOME, KC_UP,  KC_END,  KC_PSCR,  KC_TRNS,  KC_TRNS,  KC_TRNS,  \
       KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RGHT, KC_PGDN,  KC_TRNS,  KC_PGDN,  KC_LEFT, KC_DOWN,KC_RGHT, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  \
       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,KC_VOLD, KC_VOLU,  KC_MUTE,  KC_TRNS,  KC_TRNS,  \
       KC_TRNS,  KC_TRNS,  KC_TRNS,                     KC_TRNS,                                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS \
@@ -25,10 +25,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 enum function_id {
 	SHIFT_ESC,
+	SHIFT_HOME,
+	SHIFT_END,
 };
 
 const uint16_t PROGMEM fn_actions[] = {
 	[0] = ACTION_FUNCTION(SHIFT_ESC),
+	[1] = ACTION_FUNCTION(SHIFT_HOME),
+	[2] = ACTION_FUNCTION(SHIFT_END),
 };
 
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
@@ -50,6 +54,46 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
 					send_keyboard_report();
 				} else {
 					del_key(KC_ESC);
+					send_keyboard_report();
+				}
+			}
+			break;
+		case SHIFT_HOME:
+			shift_mask = get_mods()&MODS_CTRL_MASK;
+			if (record->event.pressed) {
+				if (shift_mask) {
+					add_key(KC_PGUP);
+					send_keyboard_report();
+				} else {
+					add_key(KC_HOME);
+					send_keyboard_report();
+				}
+			} else {
+				if (shift_mask) {
+					del_key(KC_PGUP);
+					send_keyboard_report();
+				} else {
+					del_key(KC_HOME);
+					send_keyboard_report();
+				}
+			}
+			break;
+		case SHIFT_END:
+			shift_mask = get_mods()&MODS_CTRL_MASK;
+			if (record->event.pressed) {
+				if (shift_mask) {
+					add_key(KC_PGDN);
+					send_keyboard_report();
+				} else {
+					add_key(KC_END);
+					send_keyboard_report();
+				}
+			} else {
+				if (shift_mask) {
+					del_key(KC_PGDN);
+					send_keyboard_report();
+				} else {
+					del_key(KC_END);
 					send_keyboard_report();
 				}
 			}
